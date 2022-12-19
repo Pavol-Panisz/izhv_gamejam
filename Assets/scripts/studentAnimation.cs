251eaf215962174df5d07f95a6555654e554f97b
Pavol_P <pavol.panisz@gmail.com>
2022-12-19 23:33:36 +0100

    created proper player and student

--- Assets/scripts/studentAnimation.cs
+++ Assets/scripts/studentAnimation.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class studentAnimation : MonoBehaviour
 {
     public float movespeed;
+    public NavMeshAgent agent;
 
     public Animator animator;
     void Start()
     {
-        
+        // TODO: 
+        // set random Animator Controller and a random sprite renderer color
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("speedy", 0);
-        animator.SetFloat("speedx", 0);
-        if (Input.GetKey("d"))
+        animator.SetFloat("speedy", agent.velocity.x);
+        animator.SetFloat("speedx", agent.velocity.y);
+        /*if (Input.GetKey("d"))
         {
             animator.SetFloat("speedy",2);
             transform.position = transform.position + transform.right * movespeed * Time.deltaTime;
@@ -36,6 +39,6 @@ public class studentAnimation : MonoBehaviour
         {
             animator.SetFloat("speedx", -2);
             transform.position = transform.position - transform.up * movespeed * Time.deltaTime;
-        }
+        }*/
     }
 }
